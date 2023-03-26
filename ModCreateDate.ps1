22e550020e240efef299177a4c239cc00feff390
Benjamin Mamistvalov <56043333+ben18mk@users.noreply.github.com>
2023-03-26 14:40:28 +0300

    Add files via upload

---
+++ ModCreateDate.ps1
@@ -0,0 +1,18 @@
+$path = Read-Host "Path";
+$fileOrFolder = Read-Host "Is it a file [f] or a folder [F]";
+$date = Read-Host "New date (hour in 24h cycle) [DD/MM/YYYY hh:mm:ss]";
+
+$date = [datetime]::parseexact($date, 'dd/MM/yyyy HH:mm:ss', $null);
+
+if (($fileOrFolder -eq "F") -Or ($fileOrFolder.ToLower() -eq "folder"))
+{
+	[System.IO.Directory]::SetCreationTime($path, $date);
+}
+elseif (($fileOrFolder -eq "f") -Or ($fileOrFolder.ToLower() -eq "file"))
+{
+	[System.IO.File]::SetCreationTime($path, $date);
+}
+else
+{
+	Write-Host "[!] I don't know whether it is a file or a folder";
+}
